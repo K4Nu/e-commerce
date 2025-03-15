06552900c50830f536e3f889b5e34d7a0d51def9
K4 <k4nu420@gmail.com>
2025-03-15 20:16:14 +0100

    Category model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models import Q
 
-# Create your models here.
+class CategoryManager(models.Manager):
+    def roots(self):
+        return self.filter(parent__isnull=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='children',
+        null=True,
+        blank=True,
+        db_index=True  # Django creates an index for FKs by default, but we can specify this explicitly.
+    )
+    objects = CategoryManager()
+
+    def __str__(self):
+        names = []
+        current = self
+        while current:
+            names.append(current.name)
+            current = current.parent
+        return '>'.join(reversed(names))
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=('parent_id',), name='category_parent_idx'),
+        ]
